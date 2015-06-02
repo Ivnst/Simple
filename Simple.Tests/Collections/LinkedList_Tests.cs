09a2820f1489d620ed05268026e7cd4a25646f01
Ivnst <for_spaming@bk.ru>
2015-06-02 16:50:18 +0300

    - Added LinkedList example.

---
+++ Simple.Tests/Collections/LinkedList_Tests.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple.Sorting;
+using Simple.Collections;
+
+namespace Simple.Tests.Collections
+{
+    [TestClass]
+    public class LinkedList_Tests
+    {
+        #region <Methods>
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void LinkedList_Creation()
+        {
+            //creation
+            var test = new LinkedList<int>();
+
+            Assert.AreEqual(0, test.Count);
+
+            //add first
+            test.AddFirst(1);
+
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(1, test[0]);
+
+            //Clear all
+            test.Clear();
+
+            Assert.AreEqual(0, test.Count);
+
+            //Add last
+            test.AddLast(2);
+
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(2, test[0]);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void LinkedList_AddSeveral()
+        {
+            //creation
+            var test = new LinkedList<int>();
+
+            Assert.AreEqual(0, test.Count);
+
+            //add items
+            test.AddFirst(2);
+            test.AddFirst(1);
+            test.AddLast(3);
+            test.AddLast(4);
+            test.AddLast(5);
+
+            Assert.AreEqual(5, test.Count);
+
+            //test foreach
+            foreach (var i in test)
+            {
+                Assert.AreEqual(test[i - 1], i);
+            }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Assert.IsTrue(test.Contains(i));
+                Assert.IsFalse(test.Contains(i*10));
+
+                Assert.AreEqual(i - 1, test.IndexOf(i));
+                Assert.AreEqual(-1, test.IndexOf(i*10));
+            }
+
+            Assert.IsFalse(test.Contains(0));
+            Assert.IsFalse(test.Contains(6));
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void LinkedList_Removing()
+        {
+            //creation
+            var test = new LinkedList<int>();
+
+            //add items
+            var fillItems = new Action<LinkedList<int>>(ints =>
+            {
+                test.AddFirst(1);
+                test.AddLast(2);
+                test.AddLast(3);
+            });
+
+            fillItems(test);
+            Assert.AreEqual(3, test.Count);
+
+            //check remove first
+            test.RemoveFirst();
+            Assert.AreEqual(2, test.Count);
+            Assert.AreEqual(2, test[0]);
+            Assert.AreEqual(3, test[1]);
+            
+            test.RemoveFirst();
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(3, test[0]);
+
+            test.RemoveFirst();
+            Assert.AreEqual(0, test.Count);
+
+            //check remove last
+            fillItems(test);
+            test.RemoveLast();
+            Assert.AreEqual(2, test.Count);
+            Assert.AreEqual(1, test[0]);
+            Assert.AreEqual(2, test[1]);
+
+            test.RemoveLast();
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(1, test[0]);
+            
+            test.RemoveLast();
+            Assert.AreEqual(0, test.Count);
+
+
+            //check deleting first
+            fillItems(test);
+            test.RemoveAt(0);
+            Assert.AreEqual(2, test.Count);
+            Assert.AreEqual(2, test[0]);
+            Assert.AreEqual(3, test[1]);
+            
+            test.RemoveAt(0);
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(3, test[0]);
+
+            test.RemoveAt(0);
+            Assert.AreEqual(0, test.Count);
+
+            //check deleting last
+            fillItems(test);
+            test.RemoveAt(2);
+            Assert.AreEqual(2, test.Count);
+            Assert.AreEqual(1, test[0]);
+            Assert.AreEqual(2, test[1]);
+
+            test.RemoveAt(1);
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(1, test[0]);
+
+            test.RemoveAt(0);
+            Assert.AreEqual(0, test.Count);
+
+            //check deleting particular item
+            fillItems(test);
+            test.Remove(2);
+            Assert.AreEqual(2, test.Count);
+            Assert.AreEqual(1, test[0]);
+            Assert.AreEqual(3, test[1]);
+
+            test.Remove(3);
+            Assert.AreEqual(1, test.Count);
+            Assert.AreEqual(1, test[0]);
+
+            test.Remove(1);
+            Assert.AreEqual(0, test.Count);
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void LinkedList_Exceptions()
+        {
+            //yes, I know about ExpectedException attribute
+
+            var test = new LinkedList<int>();
+
+            try
+            {
+                var val = test[0];
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            try
+            {
+                var val = test[-1];
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            try
+            {
+                test.RemoveAt(-1);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            try
+            {
+                test.RemoveAt(100);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            try
+            {
+                test.RemoveFirst();
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+
+            try
+            {
+                test.RemoveLast();
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+        }
+
+
+        #endregion
+    }
+}
---
+++ Simple/Collections/LinkedList.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Collections
+{
+    /// <summary>
+    /// Пример связанного списка
+    /// Example of a Linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedList<T> : IEnumerable<T> where T: IEquatable<T>
+    {
+        #region <C-tor>
+
+        public LinkedList()
+        {
+            
+        }
+
+        #endregion
+
+        #region <Fields>
+
+        private LinkedListItem<T> head = null;
+        private LinkedListItem<T> tail = null; 
+
+        #endregion
+
+        #region <Properties>
+
+        public int Count
+        {
+            get
+            {
+                var result = 0;
+                foreach (var item in this)
+                    result++;
+                return result;
+            }
+        }
+
+        public T this[int idx]
+        {
+            get
+            {
+                if(idx < 0) throw new IndexOutOfRangeException();
+                var index = 0;
+                foreach (var item in this)
+                {
+                    if (index == idx) return item;
+                    index++;
+                }
+
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        #endregion
+
+        #region <Methods>
+
+        public void AddFirst(T value)
+        {
+            if (head == null)
+            {
+                head = new LinkedListItem<T>(value);
+                tail = head;
+                return;
+            }
+
+            var newHead = new LinkedListItem<T>(value) { Next = head };
+            head = newHead;
+        }
+
+        public void AddLast(T value)
+        {
+            if (tail == null)
+            {
+                tail = new LinkedListItem<T>(value);
+                head = tail;
+                return;
+            }
+
+            var newTail = new LinkedListItem<T>(value);
+            tail.Next = newTail;
+            tail = newTail;
+        }
+
+        public void Clear()
+        {
+            head = tail = null;
+        }
+
+        public bool Contains(T value)
+        {
+            foreach (var item in this)
+            {
+                if (item.Equals(value)) return true;
+            }
+            return false;
+        }
+
+        public int IndexOf(T value)
+        {
+            var idx = 0;
+            foreach (var item in this)
+            {
+                if (item.Equals(value)) return idx;
+                idx++;
+            }
+            return -1;
+        }
+
+        public T RemoveFirst()
+        {
+            if(head == null) throw new IndexOutOfRangeException();
+
+            var val = head.Value;
+
+            if (head == tail)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+            }
+            return val;
+        }
+
+        public T RemoveLast()
+        {
+            if (tail == null) throw new IndexOutOfRangeException();
+
+            if (head == tail)
+            {
+                var val = tail.Value;
+                head = tail = null;
+                return val;
+            }
+            else
+            {
+                var curr = head;
+                var prev = head;
+
+                while (curr != null)
+                {
+                    if (curr.Next == null)
+                    {
+                        tail = prev;
+                        tail.Next = null;
+                        return curr.Value;
+                    }
+
+                    prev = curr;
+                    curr = curr.Next;
+                }
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        public T RemoveAt(int position)
+        {
+            if (position == 0)
+                return RemoveFirst();
+
+            var idx = 0;
+            var curr = head;
+            var prev = head;
+
+            while (curr != null)
+            {
+                if (idx == position)
+                {
+                    prev.Next = curr.Next;
+                    if (tail == curr)
+                    {
+                        tail = prev;
+                    }
+                    return curr.Value;
+                }
+
+                prev = curr;
+                curr = curr.Next;
+                idx++;
+            }
+
+            throw new IndexOutOfRangeException();
+        }
+
+        public void Remove(T value)
+        {
+            var curr = head;
+            LinkedListItem<T> prev = null;
+
+            while (curr != null)
+            {
+                if (curr.Value.Equals(value))
+                {
+                    if (prev == null)
+                    {
+                        head = curr.Next;
+                        return;
+                    }
+
+                    prev.Next = curr.Next;
+
+                    if (curr == tail)
+                    {
+                        tail = prev;
+                    }
+
+                    return;
+                }
+
+                prev = curr;
+                curr = curr.Next;
+            }
+
+        }
+
+        #endregion
+
+        #region <inner item>
+
+        public class LinkedListItem<T>
+        {
+            internal LinkedListItem(T item)
+            {
+                this.Value = item;
+            }
+
+            public T Value { get; set; }
+            public LinkedListItem<T> Next { get; set; }
+        }
+
+        #endregion
+
+        #region <IEnumerable>
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new LinkedListEnumerator<T>(head);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new LinkedListEnumerator<T>(head);
+        }
+
+        #endregion
+
+        #region <IEnumerator>
+
+        public class LinkedListEnumerator<T> : IEnumerator<T>
+        {
+            public LinkedListEnumerator(LinkedListItem<T> item)
+            {
+                this.first = item;
+            }
+
+            private LinkedListItem<T> first;
+            private LinkedListItem<T> current;
+
+            public T Current
+            {
+                get { return this.current.Value; }
+            }
+
+            public void Dispose()
+            {
+                this.first = this.current = null;
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { return this.current.Value; }
+            }
+
+            public bool MoveNext()
+            {
+                if (first == null) return false;
+                
+                if (current == null)
+                {
+                    current = first;
+                    return true;
+                }
+
+                if (current.Next == null) return false;
+                this.current = this.current.Next;
+                return true;
+            }
+
+            public void Reset()
+            {
+                this.current = this.first;
+            }
+        }
+
+        #endregion
+    }
+}
